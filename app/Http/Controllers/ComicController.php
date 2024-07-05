cc969ab95a5dcc44d70d098d89d4b600c39e206a
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-05 16:38:52 +0200

    M2 (1. OK)

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,7 +36,7 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return "Dati ricevuti!";
     }
 
     /**
--- resources/views/Comics/form.blade.php
+++ resources/views/Comics/form.blade.php
@@ -3,38 +3,43 @@
 @section('content')
 
 <h2>Crea un nuovo fumetto</h2>
-<form>
+<form action="{{route('comics.store')}}" method="POST">
+
     @csrf
-    <div class="mb-2">
-        <label for="title" class="form-label p-2">Titolo:</label>
-        <input type="text" class="form-control" name="title" id="title">
+
+    <div class="form-container">
+    <div class="m-4">
+        <label for="title" class="form-label p-2" >Titolo:</label>
+        <input type="text" class="form-control w-100" name="title" id="title">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="description" class="form-label p-2">Descrizione:</label>
-        <input type="text" class="form-control" name="description" id="description">
+        <input type="text" class="form-control w-100" name="description" id="description">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="thumb" class="form-label p-2">Immagine:</label>
-        <input type="text" class="form-control" name="thumb" id="thumb">
+        <input type="text" class="form-control w-100" name="thumb" id="thumb">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="price" class="form-label p-2">Prezzo:</label>
-        <input type="text" class="form-control" name="price" id="price">
+        <input type="text" class="form-control w-100" name="price" id="price">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="series" class="form-label p-2">Serie:</label>
-        <input type="text" class="form-control" name="series" id="series">
+        <input type="text" class="form-control w-100" name="series" id="series">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="sale_date" class="form-label p-2">Date di uscita:</label>
-        <input type="text" class="form-control" name="sale_date" id="sale_date">
+        <input type="text" class="form-control w-100" name="sale_date" id="sale_date">
     </div>
-    <div class="mb-2">
+    <div class="m-4">
         <label for="type" class="form-label p-2">Tipo:</label>
-        <input type="text" class="form-control" name="type" id="type">
+        <input type="text" class="form-control w-100" name="type" id="type">
     </div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+    <button type="submit" class="btn btn-primary m-4">CREA</button>
 
   </form>
 
