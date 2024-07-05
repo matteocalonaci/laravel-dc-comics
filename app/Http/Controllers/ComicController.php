0da2341a9c9c7e30b6fbbe14a9b1cde8d5b8dabf
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-05 17:17:32 +0200

    M2 (completa inserisce i fumetto perfettamente con img e informazioni ma se lo cerco come comics/13 il risultato è card vuota)

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,16 +36,30 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        return "Dati ricevuti!";
+        //sono i dati che attivano dal form
+        $data = $request->all();
+
+        $newComic = new Comic();
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+
+        $newComic->save();
+
+        return redirect()->route('comics.show', $newComic->id) ;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Comic $comic)
+    public function show(Comic $data)
     {
         $data = [
-            "comic" => $comic
+            "comic" => $data
         ];
         return view('comics.show', $data);
     }
